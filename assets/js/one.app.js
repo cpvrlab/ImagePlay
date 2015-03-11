4f6cc77dbc8bd7aff1917edd92147a1da7e8d592
Kaspar Schmid <smk2@bfh.ch>
2015-03-11 10:32:29 +0100

    removed unnecessary files, added some screenshots

--- assets/js/one.app.js
+++ assets/js/one.app.js
@@ -75,6 +75,24 @@ var App = function () {
                 jQuery('.parallaxBg').parallax("50%", 0.4);
             });
         },
+		
+        initCarousel: function () {
+			$("#carousel").owlCarousel({
+
+				navigation : true, // Show next and prev buttons
+				slideSpeed : 300,
+				paginationSpeed : 400,
+				singleItem:true
+
+				// "singleItem:true" is a shortcut for:
+				// items : 1, 
+				// itemsDesktop : false,
+				// itemsDesktopSmall : false,
+				// itemsTablet: false,
+				// itemsMobile : false
+
+			});
+        },
 
     };
 
